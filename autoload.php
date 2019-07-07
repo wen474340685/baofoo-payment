17d1bcd2bccdb7380a9a567046191bafb2d65c4a
wenqiang <474340685@qq.com>
2019-07-07 10:27:16 +0800

    add autoload

---
+++ autoload.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This file is part of the Baofoo package.
+ *
+ * (c) Moyang <474340685@qq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require __DIR__.'/src/Autoloader.php';
+
+Baofoo\Autoloader::register();
